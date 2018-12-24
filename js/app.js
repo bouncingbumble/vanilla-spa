c436f289c33a0650330897fc3c08591078044d4c
Jordan Boudreau <jordboudreau@gmail.com>
2018-12-23 23:36:56 -0800

    add router and main js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+'user strict';
+
+(function () {
+    function init() {
+        var router = new Router([
+            new Route('home', 'home.html', true),
+            new Route('about', 'about.html')
+        ]);
+    }
+    init();
+}());
